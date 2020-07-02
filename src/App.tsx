17d8042fa34a8c6f4e3cd317ae7cbda778b884bf
FranciscoN3to <franciscojf@gmail.com>
2020-07-02 12:48:08 -0300

    criação de cadastros de pontos de coleta

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
- 
-import './App.css';
+import React from 'react'
+import './App.css'
 
-import Header from './Header'
+ import Routes from './Routes'
 
 function App() {
   return (
-    <div>
-      <Header title="" />
-    </div>
-    
-  );
+    <Routes />
+  )
 }
 
-export default App;
+export default App
+ 
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,1 +1,16 @@
+import { Route, BrowserRouter} from 'react-router-dom';
 import React from 'react';
+
+import Home from './pages/Home'
+import CreatePointe from './pages/CreatePointe'
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Route component={Home} path="/" exact /> 
+            <Route component={CreatePointe} path="/create-point" /> 
+        </BrowserRouter>
+    )
+}
+
+export default Routes
